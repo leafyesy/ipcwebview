2f3ef7dd7c14614d96f4d96b074f9c3a3dc14414
shouyouye <shouyouye@tencent.com>
2022-12-05 23:16:40 +0800

    优化demo

--- app/src/main/java/com/ysydhc/ipcwebview/BaseApplication.java
+++ app/src/main/java/com/ysydhc/ipcwebview/BaseApplication.java
@@ -1,12 +1,10 @@
 package com.ysydhc.ipcwebview;
 
 import android.app.Application;
-
 import com.ysydhc.commonlib.ProcessUtil;
 import com.ysydhc.interfaceipc.InterfaceIPCInitiator;
 import com.ysydhc.interfaceipc.RemoteCallbackSupportInitiator;
 import com.ysydhc.ipcscaffold.initiator.BinderInitiatorManager;
-import com.ysydhc.ipcwebview.ipc.RemoteTestInitiator;
 import com.ysydhc.ipcwebview.ipc.RemoteWebViewInitiator;
 
 public class BaseApplication extends Application {
@@ -18,11 +16,11 @@ public class BaseApplication extends Application {
         if (ProcessUtil.getCurrentProcessName(this).endsWith("tool")) {
             BinderInitiatorManager.getInstance().addTask(new InterfaceIPCInitiator());
             BinderInitiatorManager.getInstance().addTask(new RemoteCallbackSupportInitiator());
-            BinderInitiatorManager.getInstance().addTask(new RemoteTestInitiator());
             BinderInitiatorManager.getInstance().addTask(new RemoteWebViewInitiator());
         } else {
             BinderInitiatorManager.getInstance().addTask(new InterfaceIPCInitiator());
             BinderInitiatorManager.getInstance().addTask(new RemoteCallbackSupportInitiator());
+            BinderInitiatorManager.getInstance().addTask(new RemoteWebViewInitiator());
         }
     }
 }
--- app/src/main/java/com/ysydhc/ipcwebview/MainActivity.kt
+++ app/src/main/java/com/ysydhc/ipcwebview/MainActivity.kt
@@ -22,9 +22,7 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
 
-    private var interfaceProxy: InterfaceProxy<ITest>? = null
     private var webViewImageReaderView: WebViewImageReaderView? = null
-    private var test: ITest? = null
     private var isSetJsBridge: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +31,12 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.btn1).setOnClickListener {
             IPCInitiator.startRemote(this)
         }
-        findViewById<View>(R.id.create_remote).setOnClickListener {
-            createRemoteObjectAndConnect()
-            createRemoteProxy()
+        findViewById<View>(R.id.create_remote_webview).setOnClickListener {
+            if (webViewImageReaderView != null) {
+                return@setOnClickListener
+            }
+            webViewImageReaderView = WebViewImageReaderView(this)
+            findViewById<ViewGroup>(R.id.remote_webview).addView(webViewImageReaderView)
         }
         findViewById<View>(R.id.set_callback).setOnClickListener {
             if (!isSetJsBridge) {
@@ -43,32 +44,13 @@ class MainActivity : AppCompatActivity() {
                     LogUtil.i(TAG, "url: $it")
                     true
                 }
-                //isSetJsBridge = true
+                isSetJsBridge = true
             }
+        }
+        findViewById<View>(R.id.load_url).setOnClickListener {
             webViewImageReaderView?.loadUrl("https://www.baidu.com")
         }
-        findViewById<View>(R.id.call_remote).setOnClickListener {
-            if (webViewImageReaderView != null) {
-                return@setOnClickListener
-            }
-            webViewImageReaderView = WebViewImageReaderView(this)
-            findViewById<ViewGroup>(R.id.remote_webview).addView(webViewImageReaderView)
 
-        }
-
-
-    }
-
-    private fun createRemoteObjectAndConnect() {
-        val connectBinder = RemoteServicePresenter.getInstance()
-            .queryBinderByCode<IObjectConnect>(InterfaceIPCConst.BINDER_CODE_OBJ_CONNECT)
-        connectBinder.connect(ConnectCell(ITest.KEY_CONNECT))
-    }
-
-    private fun createRemoteProxy() {
-        interfaceProxy = InterfaceProxy(ITest.KEY_CONNECT, ITest::class.java)
-        InterfaceIpcHub.getInstance().putIpcImpl(ITest.KEY_CONNECT, interfaceProxy)
-        test = interfaceProxy?.createProxy()
     }
 
 
